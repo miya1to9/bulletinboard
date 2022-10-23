7854339dd234cc0e5549d785fe8d7d40968094b9
Kosei Miyano <miya.1to9.k@gmail.com>
2022-10-23 16:58:41 +0900

    WIP:forst commit

---
+++ src/ThreadList.js
@@ -0,0 +1,19 @@
+// DO NOT DELETE
+import * as React from "react";
+
+export const ThreadListContainer = () => {
+  const [threads, setthreads] = React.useState(0);
+  fetch("https://railway-react-bulletin-board.herokuapp.com/threads?offset=0")
+    .then((response) => response.json())
+    .then((data) => {
+      setthreads(Object.values(data[0]));
+    });
+  // console.log(Object.values(data[0]));
+  // setDogURl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")
+  return (
+    //<BreedsSelect
+    <>
+      <div>{threads}</div>
+    </>
+  );
+};
